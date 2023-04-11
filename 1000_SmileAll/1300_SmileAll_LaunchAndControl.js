58a76d2ef3320ed33612d5a8e0b5662db3100634
fortunespread <130097926+fortunespread@users.noreply.github.com>
2023-04-12 03:57:05 +0900

    Delete 1000_SmileAll directory

--- 1000_SmileAll/1300_SmileAll_LaunchAndControl.js
+++
@@ -1,8 +0,0 @@
-// 'use strict'
-
-cellSmileAll.setInitialStatus();
-cellSmileAll.try();
-fieldSmileAll.show(cellSmileAll.dataToShowField);
-infoSmileAll.preshow();
-infoSmileAll.show(cellSmileAll);
-switchTextOfBtnRetry(false);
