a1c03dccea74879a0394f3fc1b32e27e7724648b
fortunespread <130097926+fortunespread@users.noreply.github.com>
2023-04-12 03:57:15 +0900

    Delete 2000_AngrySweeper directory

--- 2000_AngrySweeper/2300_AngrySweeper_LaunchAndControl.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-cellAngrySweeper.setInitialStatus();
-cellAngrySweeper.try();
-
-fieldAngrySweeper.show(cellAngrySweeper.dataToShowField);
-infoAngrySweeper.preshow();
-infoAngrySweeper.show(cellAngrySweeper.dataToShowInfo);
-// switchTextOfBtnRetry(false);
